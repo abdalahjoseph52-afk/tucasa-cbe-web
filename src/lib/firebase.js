7b90b80894700c84b69f13769f4bf3136a307198
abdalahwambura80-cmd <abdalahwambura80@gmail.com>
2025-12-08 09:40:25 +0300

    Add env config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,22 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your Configuration
+// Sasa tunasoma siri kutoka kwenye Environment Variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDqgBqJA3wXn4ZX2_CussPGN6aZrFq6PCE",
-  authDomain: "tucasa-cbe-web.firebaseapp.com",
-  projectId: "tucasa-cbe-web",
-  storageBucket: "tucasa-cbe-web.firebasestorage.app",
-  messagingSenderId: "595707335062",
-  appId: "1:595707335062:web:f5f1b47490c587a2f78db7",
-  measurementId: "G-KFHECSR6PQ"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export Services (Database & Auth ONLY)
-export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
